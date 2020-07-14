e1fdfc7b0fe72d74bab7db61ef1ad70035e89ab7
Joona Piirainen <joona.piirainen@gmail.com>
2020-07-14 13:10:02 +0300

    remove unnecessary settingsNav

--- src/components/Settings/Misc/MiscSettings.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const MiscSettings = () => {
-    return <h1>Some awesome settings</h1>
-}
-
-export default MiscSettings
--- src/components/Settings/index.js
+++ src/components/Settings/index.js
@@ -1,23 +1,10 @@
 import React from 'react'
-import { Switch } from 'react-router-dom'
-import PrivateRoute from '../PrivateRoute'
-import SettingsNav from './SettingsNav/SettingsNav'
 import ChangeInfoForm from './Form/ChangeInfoForm'
-import MiscSettings from './Misc/MiscSettings'
-import { Divider } from '@material-ui/core'
 
 const Settings = () => {
     return (
         <>
-            <SettingsNav />
-            <Divider />
-            <Switch>
-                <PrivateRoute
-                    path="/settings/changeinfo"
-                    component={ChangeInfoForm}
-                />
-                <PrivateRoute path="/settings/misc" component={MiscSettings} />
-            </Switch>
+            <ChangeInfoForm />
         </>
     )
 }
